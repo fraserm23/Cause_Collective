7e89d75453ffcdf220dbd4d68e86738b85e180ca
fraserm23 <79196963+fraserm23@users.noreply.github.com>
2021-04-04 19:46:06 +0100

    Add project files.

---
+++ PFTW CW2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PFTW_CW2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
